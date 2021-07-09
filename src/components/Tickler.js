692d3137f411f7e65e23c971de786d792b4b7cdf
steffent1493 <steffent1493@gmail.com>
2021-07-09 13:17:44 -0400

    mission accomplished

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Tickler() {
-  function tickle(event) {
-    console.log(event);
-  }
-
-  return <button onClick={tickle}>Tickle me!</button>;
+  return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>
 }
 
 export default Tickler;
